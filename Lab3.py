f57c875a80fca11c0c5cae8df069b3329228fb4c
Ishmaam Iftekhar Khan <ishmaam@iut-dhaka.edu>
2025-03-10 05:55:13 +0600

    Started Lab 3

---
+++ Lab3.py
@@ -0,0 +1,7 @@
+import sys #allows system 
+import socket #for socket module to execute commands like recv, send, socket connection
+import getopt #for parsing the command line- -l, -a, -t stands for what
+import threading #for spawning new threads so the processor doesn't get blocked by one process
+import subprocess #for getting, sending and storing output
+
+
